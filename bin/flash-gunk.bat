a471323d1a8a4b84efa536e7031d3367a90b1ba0
codingpeanut <codingpeanut123@gmail.com>
2024-10-05 11:01:41 +0800

    add new file

--- bin/flash-gunk.bat
+++ bin/flash-gunk.bat
@@ -1,3 +1,3 @@
 :: 使用 plink 連接到 Telnet 並執行命令
-PuTTY\plink -telnet localhost -P 4444 < scripts\commands.txt
+PuTTY\plink -telnet localhost -P 4444 < scripts\flash.txt
 pause
---
+++ bin/reflash-gunk.bat
@@ -0,0 +1,3 @@
+:: 使用 plink 連接到 Telnet 並執行命令
+PuTTY\plink -telnet localhost -P 4444 < scripts\reflash.txt
+pause
